3c1bdbdde3549e92c5477e551ccf17daef883ce4
0bera1 <ahmedberaekimci@gmail.com>
2025-04-21 15:02:52 +0300

    kullanılmayan dosyalar silindi

--- server/dist/validation/validation.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ValidationService {
-    validateEmail(email: string): boolean;
-    validatePassword(password: string): boolean;
-    validateName(name: string): boolean;
-    validateBalance(balance: number): boolean;
-    validateCardNumber(cardNumber: string): boolean;
-    validateIban(iban: string): boolean;
-}
