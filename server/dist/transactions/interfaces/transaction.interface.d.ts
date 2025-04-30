3413ac4ca54e8dc75191fb76d165cbc416867280
0bera1 <ahmedberaekimci@gmail.com>
2025-04-30 12:31:46 +0300

    updated

---
+++ server/dist/transactions/interfaces/transaction.interface.d.ts
@@ -0,0 +1,11 @@
+export interface Transaction {
+    id: number;
+    sender_id: number;
+    receiver_id: number;
+    amount: number;
+    currency: string;
+    description?: string;
+    status: string;
+    created_at: Date;
+    updated_at: Date;
+}
